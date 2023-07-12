3540fc194151055ab434fe115d05c506dc21b8bf
Zeemyo <zidanalam77@gmail.com>
2023-07-12 12:54:36 +0700

    Code files added

--- Chapter05/h4_01_rnn_with_lstm_gru/main.go
+++ Chapter05/h4_01_rnn_with_lstm_gru/main.go
@@ -12,8 +12,26 @@ import (
 	"gorgonia.org/tensor"
 )
 
-var cpuprofile = flag.String("cpuprofile", "", "I3")
-var memprofile = flag.String("memprofile", "", "16GB")
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+var memprofile = flag.String("memprofile", "", "write memory profile to this file")
+
+// TODO/Questions:
+
+// Input:
+// extend vocab.go to import txt
+//  - got ur dict got ur vocab got ur vocabindex
+//  - replace sentencesRaw
+//  - slice into chars. map to []rune
+//  - produce paired input/output examples (input, input + 1 across corpus)
+//  - 1HV the lot? sparsity meh?
+
+// Model:
+// - why keySize = len of keys +2?
+// - definition of ValueGrad?
+
+//  Main:
+//  - keys/durations/mOut?
+//  - why pointer for trainiter?
 
 const (
 	embeddingSize = 20
@@ -26,17 +44,22 @@ const (
 )
 
 var trainiter = flag.Int("iter", 5, "How many iterations to train")
-var inputFile = flag.String("filename", "inputan.txt", "Filename of text to train on")
+var inputFile = flag.String("filename", "shakespeare.txt", "Filename of text to train on")
 
 // various global variable inits
 var epochSize = -1
 var inputSize = -1
 var outputSize = -1
 
+// const corpus string = "shakespeare.txt"
+// const corpus string = `the cat sat on the mat
+// hello world
+// wild stalyns
+
 var corpus string
 
 func init() {
-	buf, err := ioutil.ReadFile("inputan.txt")
+	buf, err := ioutil.ReadFile("shakespeare.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -44,6 +67,20 @@ func init() {
 }
 
 var dt tensor.Dtype = tensor.Float32
+
+// type trainingPair string
+
+// type pair struct {
+// 	t string
+// 	tplusone string
+// }
+
+// type trainingPair struct {
+// 	in, out []message
+// }
+
+// WHERE TO 1HV THE INPUITS?????
+// func OneHotVector(id, classes int, t tensor.Dtype, opts ...NodeConsOpt) *Node {
 
 func main() {
 	flag.Parse()
@@ -55,6 +92,17 @@ func main() {
 		pprof.StartCPUProfile(f)
 		defer pprof.StopCPUProfile()
 	}
+	// f, err := os.Create("trace.out")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer f.Close()
+
+	// err = trace.Start(f)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer trace.Stop()
 
 	hiddenSize := 100
 
